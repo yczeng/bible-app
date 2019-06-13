21c9102484ff2afd4155c76b476df04049e208a7
Zachary Pitcher <zackpi@mit.edu>
2019-06-13 15:45:16 -0400

    changed app name

--- app/src/main/java/com/android/biblio/app/MainActivity.java
+++ app/src/main/java/com/android/biblio/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.android.bibleapp2;
+package com.android.biblio.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.android.biblio.app.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
