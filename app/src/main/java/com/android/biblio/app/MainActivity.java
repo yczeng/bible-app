7bd2ee8ce872c1fad11422dae7b72bff78c7661d
Catherine Zeng <yczeng@mit.edu>
2019-06-14 15:26:04 -0400

    shitty pop up working

--- app/src/main/java/com/android/biblio/app/MainActivity.java
+++ app/src/main/java/com/android/biblio/app/MainActivity.java
@@ -5,20 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListPopupWindow;
+import android.widget.PopupMenu;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
-    private Button buttonLaunchNewActivity;
     private TextView textView;
     private TextView textView1;
     Context context = this;
@@ -29,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
-        buttonLaunchNewActivity = findViewById(R.id.buttonLaunchNewActivity);
         textView = findViewById(R.id.text);
         textView1 = findViewById(R.id.textView1);
 
@@ -47,6 +54,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void bookPopUp(View view){
+        PopupMenu popup = new PopupMenu(MainActivity.this, view);
+        //Inflating the Popup using xml file
+        popup.getMenuInflater()
+                .inflate(R.menu.book_popup, popup.getMenu());
+
+        //registering popup with OnMenuItemClickListener
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(
+                        MainActivity.this,
+                        "You Clicked : " + item.getTitle(),
+                        Toast.LENGTH_SHORT
+                ).show();
+                return true;
+            }
+        });
+
+        popup.show(); //showing popup menu
     }
 
     public void moveActivity(View view) {
