4151943acb38184654fdf949bdd992405373427f
Catherine Zeng <yczeng@mit.edu>
2019-06-15 00:21:53 -0400

    cleaned up code a bit

--- app/src/main/java/com/android/biblio/app/MainActivity.java
+++ app/src/main/java/com/android/biblio/app/MainActivity.java
@@ -1,80 +1,70 @@
 package com.android.biblio.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.ListPopupWindow;
-import android.widget.PopupMenu;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import org.json.JSONArray;
 import org.json.JSONException;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
     private TextView textView;
-    private TextView textView1;
-    Context context = this;
-    Dialog bookDialog;
-    Dialog chapterDialog;
 
+    Dialog bookDialog;
     Button bookButton;
+    Dialog chapterDialog;
     Button chapterButton;
+
+    JSONArray verseList;
+    JSONBible kjv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // creates the popup windows for book and chapter
         bookDialog = new Dialog(this);
         chapterDialog = new Dialog(this);
 
-        button = findViewById(R.id.button);
         textView = findViewById(R.id.text);
 
         bookButton = findViewById(R.id.bookButton);
         bookButton.setText(GlobalVariable.getInstance().getBook());
 
         chapterButton = findViewById(R.id.chapterButton);
-        chapterButton.setText(Integer.toString(GlobalVariable.getInstance().getChapter()));
-
-        JSONArray verseList;
-        final JSONBible kjv;
-
-        verseList = readJSONArray("kjv.json");
-        kjv = new JSONBible(verseList);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String resultVerse = kjv.get(GlobalVariable.getInstance().getBook(), GlobalVariable.getInstance().getChapter(), 1);
-                textView.setText(resultVerse);
-            }
-        });
+        chapterButton.setText(String.valueOf(GlobalVariable.getInstance().getChapter()));
     }
 
+    // Generates a pop up of all books
     public void bookPopUp(View view){
         bookDialog.setContentView(R.layout.book_pop_up);
         bookDialog.show();
     }
 
+    // Updates global variable when click on new book
+    public void changeBook(View v){
+        String newBook = v.getTag().toString();
+        GlobalVariable.getInstance().setBook(newBook);
+
+        bookButton.setText(newBook);
+        bookDialog.dismiss();
+    }
+
+    // Generates a pop up of all the chapters for given book
+    public void chapterPopUp(View view){
+        chapterDialog.setContentView(R.layout.chapter_pop_up);
+        chapterDialog.show();
+    }
+
+    // Updates global variable when click on new chapter
     public void changeChapter(View v){
         int newChapter = Integer.parseInt(v.getTag().toString());
         GlobalVariable.getInstance().setChapter(newChapter);
@@ -83,28 +73,18 @@ public class MainActivity extends AppCompatActivity {
         chapterDialog.dismiss();
     }
 
-    public void chapterPopUp(View view){
-        chapterDialog.setContentView(R.layout.chapter_pop_up);
-        chapterDialog.show();
-    }
-
-    public void changeBook(View v){
-        String newBook = v.getTag().toString();
-//        Context context = getApplicationContext();
-//        CharSequence text = newBook;
-//        int duration = Toast.LENGTH_LONG;
-//        Toast toast = Toast.makeText(context, text, duration);
-//        toast.show();
-
-        GlobalVariable.getInstance().setBook(newBook);
-        bookButton.setText(newBook);
-        bookDialog.dismiss();
-    }
-
+    // Moves on to the next page with json result of verse
     public void moveActivity(View view) {
         Intent intent = new Intent(this, ReaderActivity.class);
-        startActivity(intent);
-        finish();
+
+        verseList = readJSONArray("kjv.json");
+        kjv = new JSONBible(verseList);
+
+        String resultVerse = kjv.get(GlobalVariable.getInstance().getBook(), GlobalVariable.getInstance().getChapter(), 1);
+        textView.setText(resultVerse);
+        intent.putExtra("json", resultVerse);
+//        startActivity(intent);
+//        finish();
     }
 
     // Reads a json array from a file
