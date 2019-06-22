0dedacd0ddda8d79000acfeeb44e87add86133f3
Zachary Pitcher <zackpi@mit.edu>
2019-06-22 09:44:59 -0400

    make layout for text options popup

--- app/src/main/java/com/android/biblio/app/ReaderActivity.java
+++ app/src/main/java/com/android/biblio/app/ReaderActivity.java
@@ -1,9 +1,7 @@
 package com.android.biblio.app;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
