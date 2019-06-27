6d90e9eb63140e695d1fa242120dec4dacc42911
Catherine Zeng <yczeng@mit.edu>
2019-06-27 18:21:43 -0400

    modified versePosition.json

--- scripts/getProportionVerse.py
+++ scripts/getProportionVerse.py
@@ -10,10 +10,11 @@ temp_book_result = []
 book_name = ""
 for i in kjv_data:
 	if i["book_id"] != book_name:
+		if book_name == "":
+			book_name = i["book_id"]
+			continue
 
-		if i["book_id"] not in result:
-			result[i["book_id"]] = {}
-		book = result[i["book_id"]]
+		book = {}
 
 		# all the processing for entire book should happen here
 		chapter_name = -1
@@ -54,6 +55,7 @@ for i in kjv_data:
 
 			length_passed_so_far += len(each_chapter_blah["text"])
 
+		result[book_name] = book
 
 		temp_book_result = []
 		book_name = i["book_id"]
@@ -62,12 +64,55 @@ for i in kjv_data:
 	elif i["book_id"] == book_name:
 		temp_book_result.append(i)
 
+################################# Everything from here to MOOPY is a duplicate of above
+book = {}
+
+# all the processing for entire book should happen here
+chapter_name = -1
+chap_lens = {}
+temp_chapter_len = 0
+for each_chapter_blah in temp_book_result:
+	# rest of total characters in a chapter count
+	if each_chapter_blah["chapter"] != chapter_name:
+		if chapter_name != -1:
+			chap_lens[chapter_name] = temp_chapter_len
+		temp_chapter_len = 0
+		chapter_name = each_chapter_blah["chapter"]
+
+	elif each_chapter_blah["chapter"] == chapter_name:
+		temp_chapter_len += len(each_chapter_blah["text"])
+
+chap_lens[chapter_name] = temp_chapter_len
+
+# print(chap_lens)
+
+
+length_passed_so_far = 0
+chapter_name = -1
+for each_chapter_blah in temp_book_result:
+	if each_chapter_blah["chapter"] != chapter_name:
+		length_passed_so_far = 0
+		chapter_name = each_chapter_blah["chapter"]
+
+	# this means that it doesn't exist yet
+	# i.e. chapter 1. len(book) = 0, so need to append
+	# print(len(book), i["chapter"], book)
+
+	if len(book) < each_chapter_blah["chapter"]:
+		book[str(each_chapter_blah["chapter"])] = [round(length_passed_so_far / chap_lens[each_chapter_blah["chapter"]], 4)]
+	else:
+		# this grabs the array of string verses
+		book[str(each_chapter_blah["chapter"])].append(round(length_passed_so_far / chap_lens[each_chapter_blah["chapter"]], 4))
+
+	length_passed_so_far += len(each_chapter_blah["text"])
+
+result[book_name] = book
+############################### MOOPY
 
 f = open("app/src/main/assets/versePosition.json", "w")
 f.write(str(result))
 
 # print(result)
-print((result))
 
 
 # {"chapter":20,"verse":10,"text":"Then the disciples went away again unto their own home.","book_id":"John","book_name":"John"}
